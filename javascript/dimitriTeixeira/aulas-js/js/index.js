5fd567102b34ee16d731282cbeaf9cacbf754657
eurobertacs <roberta_sousa@yahoo.com.br>
2022-10-20 22:55:17 -0300

    Produtos gerados aula 5

--- javascript/dimitriTeixeira/aulas-js/js/index.js
+++ javascript/dimitriTeixeira/aulas-js/js/index.js
@@ -1,31 +1,9 @@
-// Diferença entre texto e número
-console.log (5 + 5);
-console.log(1);
-console.log ("5 + 5"); 
-console.log("1");
+//Comentário de uma linha
+document.getElementById("texto").innerHTML = 
+    "Meu primeiro texto <b>JS</b>!";
 
-// Variável
-var a = 1;
-var b = 5;
-var c = a + b;
-var d = a * b;
-var e = a / b;
-var f = a - b;
-
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
-
-// Exemplo de nomes de variáveis que são case sensitive
-var NOME = "Fulano";
-var nome = "De Tal";
-
-console.log(NOME);
-console.log(nome);
-//console.log(Pessoa);
-
-// Exemplos de nomes de variáveis aceitos
-var primeiro_nome;
-var primeiroNome; // recomendado
-//var primeiro-nome; 
+/*
+    Comentário de múltiplas linhas
+    Autor: Fulano de Tal
+    Data: dd-mm-aaaa
+*/
