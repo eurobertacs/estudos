8bc0e9c2f82cc74d5b3b9b1a07d6d3d8687b5577
eurobertacs <roberta_sousa@yahoo.com.br>
2022-10-20 21:28:48 -0300

    Produtos gerados aula 2

--- javascript/dimitriTeixeira/aulas-js/js/index.js
+++ javascript/dimitriTeixeira/aulas-js/js/index.js
@@ -1,2 +1,4 @@
-alert('Olá mundo 2!');
-alert('Um alerta rodando de uma página externa');
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+alert (10 + 5);
+alert ("Oi isso é um alerta");
+console.log("Oi isso é um console.log");
