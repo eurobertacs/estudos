a74c66407996ea72134fd284d4adf7b628d46d84
eurobertacs <roberta_sousa@yahoo.com.br>
2022-10-20 22:03:52 -0300

    Produtos gerados aula 3

--- javascript/dimitriTeixeira/aulas-js/js/index.js
+++ javascript/dimitriTeixeira/aulas-js/js/index.js
@@ -1,4 +1,28 @@
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
-alert (10 + 5);
-alert ("Oi isso é um alerta");
-console.log("Oi isso é um console.log");
+// Declarações
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+// Outro exemplo de declaração
+let d,e,f; d=5; e=6; f=7;
+
+// espaços em brancos aceitos no javascript
+let nome="Fulano";
+let sobrenome = "de Tal" ;
+
+// Exemplos de declarações com quebras de linha
+document.getElementById("texto").innerHTML = 
+    "Meu primeiro texto <b>JS</b>";
+
+document
+    .getElementById("texto").innerHTML = 
+    "Meu primeiro texto <b>JS</b>";
+
+// Declarações em bloco
+function minhaFuncao() {
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+}
