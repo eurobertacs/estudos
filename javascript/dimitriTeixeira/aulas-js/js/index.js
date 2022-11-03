9aa3790d44e4d71973df061cb51cf818828c6c89
eurobertacs <roberta_sousa@yahoo.com.br>
2022-11-03 20:32:15 -0300

    Produtos gerados aula 8

--- javascript/dimitriTeixeira/aulas-js/js/index.js
+++ javascript/dimitriTeixeira/aulas-js/js/index.js
@@ -1,123 +1,47 @@
-/* Vamos entender operadores
--> Os operadores JavaScript são usados para atribuir valores, comparar
-valores, executar operações aritméticas e muito mais.
+/* Funções
+Uma função JavaScript é um bloco de código projetado para executar uma 
+tarefa específica.
 
-São os sinais que usamos: + - * / = ++ -- += -= && || etc...
+É como uma pequena "fábrica" onde você faz uma entrada e ele te dá uma
+saída.
 
-São separados em 6 "categorias":
+Pode ser encarado como um "mini-programas" projetados para fazer uma tarefa 
+que vai contribuir para todo código.
 
-1) Operadores aritméticos (matemáticos)
-2) Operadores de atribuição
-3) Operadores de sequencia 
-4) Operadores de comparação
-5) Operadores de Condicional (Ternário)
-6) Operadores Lógicos
-
+Uma função JavaScript é executada quando algo a invoca (chama-a).
 */
 
-var valor1, valor2, soma, sub, mult, divi, incr, decr;
-valor1 = 5;
-valor2 = 2;
+// Função de soma
+function soma(valor1, valor2){
+    return valor1 + valor2;
+}
 
-soma = valor1 + valor2;
-sub = valor1 - valor2;
-mult = valor1 * valor2;
-divi = valor1 / valor2;
-incr = ++valor1;
-decr = --valor2;
+// Função de conversão de real para dólar
+function converterRealParaDolar(real,cotacaoDolar){
+    return real * cotacaoDolar;
+}
 
-alert('Soma: ' + soma);
-alert('Subtração: ' + sub);
-alert('Multiplicação: ' + mult);
-alert('Divisão: ' + divi);
-alert('Incremento: ' + incr + ' valor1: ' + valor1);
-alert('Decremento: ' + decr + ' valor2: ' + valor2);
+document.getElementById("texto").innerHTML = soma(10,10);
 
-valor1 += valor2;
-alert('Soma: ' + valor1);
+var total = soma(10,23);
+alert(total);
 
-valor1 -= valor2; 
-alert('Subtração: ' + valor1);
+var valorReal = 7.89;
+var cotacaoDolar = 5.08;
+var valorConvertido = converterRealParaDolar(valorReal,cotacaoDolar);
 
-valor1 *= valor2; 
-alert('Multiplicação: ' + valor1);
+alert("O valor em real é R$ " + valorReal + " e o valor em dólar é U$ " + valorConvertido);
 
-valor1 /= valor2; 
-alert('Divisão: ' + valor1);
+// Exemplo preliminar de função de evento
+function alertaHello(){
+    alert("Olá pessoal");
+}
 
-valor1 = "Dimitri ";
-valor2 = "Teixeira";
+alertaHello();
 
-soma = valor1 + valor2;
+function paraCelsius(valorFahrenheit){
+    return (5 / 9) * (valorFahrenheit - 32);
+}
 
-alert('Soma de duas strings ' + soma);
-
-// Uso de operadores de comparação
-var a,b, c, d, opComp;
-
-a = 8;
-b = 10;
-c = 8;
-d = "8";
-
-opComp = (a == b);
-alert ('Resultado comparação a == b ' + opComp);
-
-opComp = (a == c);
-alert ('Resultado comparação a == c ' + opComp);
-
-opComp = (a == d);
-alert ('Resultado comparação a == d ' + opComp);
-
-opComp = (a === d);
-alert ('Resultado comparação a === d ' + opComp);
-
-opComp = (a === c);
-alert ('Resultado comparação a === c ' + opComp);
-
-opComp = (a != b);
-alert ('Resultado comparação a != b ' + opComp);
-
-opComp = (a != c);
-alert ('Resultado comparação a != c ' + opComp);
-
-opComp = (a != d);
-alert ('Resultado comparação a != d ' + opComp);
-
-opComp = (a !== d);
-alert ('Resultado comparação a !== d ' + opComp);
-
-opComp = (a !== c);
-alert ('Resultado comparação a !== c ' + opComp);
-
-opComp = (a > b);
-alert ('Resultado comparação a > b ' + opComp);
-
-opComp = (a >= b);
-alert ('Resultado comparação a >= b ' + opComp);
-
-opComp = (a < b);
-alert ('Resultado comparação a < b ' + opComp);
-
-opComp = (a <= b);
-alert ('Resultado comparação a <= b ' + opComp);
-
-// Operador Ternário
-var idade, idade2, eleitor, resultado;
-idade = 17;
-idade2 = 25;
-eleitor = (idade <= 18) ? "não eleitor" : "sim eleitor";
-alert ('A resposta é ' + eleitor + ' a idade dele é de: ' + idade);
-
-eleitor = (idade2 <= 18) ? "não eleitor" : "sim eleitor";
-alert ('A resposta é ' + eleitor + ' a idade dele é de: ' + idade2);
-
-//Operadores lógicos
-resultado = (idade > 60 && idade < 70);
-alert (resultado);
-
-resultado = (idade === 65 || idade === 72);
-alert(resultado);
-
-resultado = !(idade === 65);
-alert(resultado);
+var celsius = paraCelsius(77);
+alert("A temperatura é de " + celsius + " graus Celsius.")
